70f22581ad4e1683e5b846471a83c0266d93745d
JohnDesiderio <johnfrancisdesiderio@gmail.com>
2026-03-31 19:52:08 -0400

    Remove hyprland.nix move thru home-manager/wayland

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -3,7 +3,6 @@
   imports = [
     ./git.nix
     ./nvim.nix
-    ./hyprland.nix
     ./wayland.nix
   ];
 }
--- modules/home/hyprland.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs, lib, inputs, ... }:
-{
-  programs.hyprland.enable = true;
-  programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
-}
