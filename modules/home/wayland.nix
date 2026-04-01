56c100f7a72ebd5181b6caa28fdfa586eb5c4810
JohnDesiderio <johnfrancisdesiderio@gmail.com>
2026-03-31 20:03:04 -0400

    borders plus plus causing problems

--- modules/home/wayland.nix
+++ modules/home/wayland.nix
@@ -4,24 +4,11 @@
     enable = true;
 
     plugins = [
-      inputs.hyprland-plugins.packages."${pkgs.system}".borders-plus-plus
+
     ];
 
     settings = {
-      "plugins:borders-plus-plus" = {
-        add_borders = 1;
 
-	# you can add up to 9 borders
-	"col.border_1" = "rgb(ffffff)";
-	"col.border_2" = "rgb(2222ff)";
-
-	# -1 means "default" as in the ones defined in general:border_size
-	border_size_1 = 10;
-	border_size_2 = -1;
-
-	# makes outer edges match rounding of the parent. Turn on / off to better understand. Default = on.
-	natural_rounding = "yes";
-      }; 
     };
   };
 }
