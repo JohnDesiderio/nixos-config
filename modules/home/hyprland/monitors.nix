a59db98b36f5fed80d3b56a1f6b9fefe1da7c0f7
JohnDesiderio <johnfrancisdesiderio@gmail.com>
2026-04-20 22:14:04 -0400

    Kinda lost on how to add second monitor

--- modules/home/hyprland/monitors.nix
+++ modules/home/hyprland/monitors.nix
@@ -4,13 +4,12 @@
     settings.monitor = [ 
     	"DP-1, 1920x1080, auto, auto"
     	"HDMI-1, 1920x1080, auto, auto"
+    	"WAYLAND-1, 1920x1080, auto, auto"
     ];
 
     extraConfig = ''
-      # hyprlang noerror true
-        source = ~/.config/hypr/monitors.conf
-        source = ~/.config/hypr/workspaces.conf
-      # hyprlang noerror false 
+      monitor=HDMI-1, 1920x1080, 0x0, 1
+      monitor=DP-1, 1920x1080, 0x0, 1
     '';
   };
 
