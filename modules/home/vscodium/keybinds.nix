832d1cfcd6ce8024ac57a95859f7b838632700bd
JohnDesiderio <johnfrancisdesiderio@gmail.com>
2026-05-05 15:23:18 -0400

    Syntax error

--- modules/home/vscodium/keybinds.nix
+++ modules/home/vscodium/keybinds.nix
@@ -6,11 +6,11 @@
         key = "ctrl+q";
         command = "editor.action.commentLine";
         when = "editorTextFocus && !editorReadonly"
-      },
+      }
       {
         key = "ctrl+s";
         command = "workbench.action.files.saveFiles";
       }
     ];
-  };
+  }
 }
