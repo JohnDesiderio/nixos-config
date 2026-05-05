e9e32072a54b3f2fb5b2e007ea125235c9c473d5
JohnDesiderio <johnfrancisdesiderio@gmail.com>
2026-05-05 15:25:29 -0400

    .

--- modules/home/vscodium/keybinds.nix
+++ modules/home/vscodium/keybinds.nix
@@ -5,12 +5,12 @@
       {
         key = "ctrl+q";
         command = "editor.action.commentLine";
-        when = "editorTextFocus && !editorReadonly"
+        when = "editorTextFocus && !editorReadonly";
       }
       {
         key = "ctrl+s";
         command = "workbench.action.files.saveFiles";
       }
     ];
-  }
+  };
 }
