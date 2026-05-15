bf1841cae12d50f1d8bfb05a23084cae60937372
JohnDesiderio <johnfrancisdesiderio@gmail.com>
2026-05-15 11:02:50 -0400

    Copy paste lulE

---
+++ modules/home/vscodium/extensions/auto.nix
@@ -0,0 +1,15 @@
+# This file was written by hmm (Home Manager Manager).
+# Don't edit it manually!
+# This file should always parse as an array of string literals.
+#
+[
+  "baccata.scaladex-search"
+  "codezombiech.gitignore"
+  "disneystreaming.smithy"
+  "github.vscode-pull-request-github"
+  "mishkinf.goto-next-previous-member"
+  "redhat.vscode-yaml"
+  "timonwong.shellcheck"
+  "usernamehw.errorlens"
+  "haskell.haskell"
+]
---
+++ modules/home/vscodium/extensions/managed.nix
@@ -0,0 +1,25 @@
+#
+# This file was written by hmm (Home Manager Manager).
+# Don't edit it manually!
+# This file should always parse as an array of attrsets.
+#
+[
+  {
+    name = "metals";
+    publisher = "scalameta";
+    version = "1.55.2";
+    sha256 = "0dlxlyhqz85xj0qq2gag5crkn4lp4c476dirkg1bnccyl5jnyrd2";
+  }
+  {
+    name = "smithy";
+    publisher = "disneystreaming";
+    version = "0.0.14";
+    sha256 = "14k1fdwfhixfnmamwnzncyvg5s8433nxglrd3bwrsxs534jxlwbn";
+  }
+  {
+    name = "scaladex-search";
+    publisher = "baccata";
+    version = "0.3.5";
+    sha256 = "1cfbcpyvwyvcdczy7rxy65rvnmdh1lggv52xlpzvrlwf9al2dzkx";
+  }
+]
